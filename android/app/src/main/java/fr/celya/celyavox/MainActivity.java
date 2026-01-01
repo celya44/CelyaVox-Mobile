e2f1411e46807484686b51d7e3e8502792859c4d
root <root@freepbx17-dev.celya.fr>
2026-01-01 19:43:59 +0100

    Modification zone cliquable

--- android/app/src/main/java/fr/celya/celyavox/MainActivity.java
+++ android/app/src/main/java/fr/celya/celyavox/MainActivity.java
@@ -1,5 +1,45 @@
 package fr.celya.celyavox;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+public class MainActivity extends BridgeActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Ensure IME is shown and layout resizes properly on newer SDKs
+		getWindow().setSoftInputMode(
+			WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE | WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN
+		);
+		enableImmersiveMode();
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if (hasFocus) {
+			enableImmersiveMode();
+		}
+	}
+
+	private void enableImmersiveMode() {
+		WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+		View decorView = getWindow().getDecorView();
+		WindowInsetsControllerCompat controller = ViewCompat.getWindowInsetsController(decorView);
+		if (controller != null) {
+			controller.hide(WindowInsetsCompat.Type.systemBars());
+			controller.setSystemBarsBehavior(
+				WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+			);
+		}
+	}
+}
